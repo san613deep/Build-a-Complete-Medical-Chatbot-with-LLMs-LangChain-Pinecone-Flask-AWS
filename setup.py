3f2cb93c4a1bd86ff2727a0a6a5866d058826284
sandeep poddar <poddarsandeep063@gmail.com>
2025-12-25 16:36:20 +0530

    requiresment added

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="medical_chatbot",
+    version="0.1.0",
+    author="Sandeep poddar",
+    author_email="sandeep.poddar.ug22@nsut.ac.in",
+    packages=find_packages(),
+    install_requires=[]
+)
